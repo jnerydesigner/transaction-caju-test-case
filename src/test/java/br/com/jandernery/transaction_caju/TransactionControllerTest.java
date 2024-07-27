f348ba3e67eec518b73c4131eaa113da3010e8c8
Jander da Costa Nery <jander.webmaster@gmail.com>
2024-07-26 20:36:15 -0400

    Feat: include tests in controller

--- src/test/java/br/com/jandernery/transaction_caju/TransactionControllerTest.java
+++ src/test/java/br/com/jandernery/transaction_caju/TransactionControllerTest.java
@@ -1,2 +1,36 @@
-package br.com.jandernery.transaction_caju;public class TransactionControllerTest {
+package br.com.jandernery.transaction_caju;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class TransactionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+    @Test
+    public void testGetHelloTransaction() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/transaction"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Transaction"));
+    }
+
+    @Test
+    public void testTransactionCreate() throws Exception{
+        String payloadTransaction = "{\"account\":\"123\",\"totalAmount\":100.00, \"mcc\": \"5811\", \"merchant\": \"PADARIA DO ZE SAO PAULO BR\"}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payloadTransaction)
+        ).andExpect(status().isOk()).andExpect(content().json(payloadTransaction));
+    }
 }
